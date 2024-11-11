8311cad3d47cfee296dbc95000567ad4ec0e1728
koza <koza@transfez.com>
2024-11-11 22:59:13 +0700

    init: UI improvement

--- src/screens/second.tsx
+++ src/screens/second.tsx
@@ -1,4 +1,15 @@
+import React from "react";
 import Button from "../components/button";
+
+const container: React.CSSProperties = {
+  display: "flex",
+  gap: 8,
+  position: "fixed",
+  padding: 12,
+  backgroundColor: "white",
+  maxWidth: "calc(500px - 26px)",
+  width: "calc(80vw - 26px)",
+};
 
 const SecondScreen = ({
   text,
@@ -10,29 +21,36 @@ const SecondScreen = ({
   return (
     <div
       style={{
-        minHeight: 400,
+        minHeight: "calc(100vh - 8px)",
         display: "flex",
         flexDirection: "column",
         gap: 12,
+        border: "1px solid #EAEAEA",
         padding: "8px 0px",
+        position: "relative",
       }}
     >
-      <div>
+      <div style={{ paddingLeft: 12, ...container, top: 0 }}>
         <Button onClick={onBack}>Back</Button>
       </div>
+      <div style={{ height: 49.5 }} />
       <div style={{ backgroundColor: "#EAEAEA", padding: "8px 12px" }}>
-        <p style={{ textAlign: "center", lineHeight: "24px" }}>{text}</p>
+        <p
+          style={{
+            textAlign: "center",
+            lineHeight: "24px",
+            textOverflow: "ellipsis",
+            overflow: "hidden",
+          }}
+        >
+          {text}
+        </p>
       </div>
       <div style={{ height: 48 }} />
       <div
         style={{
-          display: "flex",
-          gap: 8,
-          position: "fixed",
+          ...container,
           bottom: 0,
-          maxWidth: 480,
-          width: "80vw",
-          padding: 10,
         }}
       >
         <Button style={{ width: "100%" }}>Play</Button>
